290e334473021a5a0fc82f4a924499bc8a28581b
Robert Pelayo <robpelayo@gmail.com>
2020-02-27 16:33:18 -0800

    Added the prompt menu and write to follows

--- rsa.py
+++ rsa.py
@@ -2,6 +2,9 @@
 
 import random
 
+UPPER_LIMIT = int(4294967295)
+LOWER_LIMIT = int(2147483648)
+
 
 # Power function at the bit level from geeksforgeeks.org
 def power(x, y, n):
@@ -54,8 +57,60 @@ def miller_rabin(n):
 
 def test_prime(n):
     # False = composite, True = prime
-    if n % 2 == 0:
+    if n % 2 == 0 or n <= 1:
         return False
     return miller_rabin(n)
 
 
+def random_big_num():
+    return random.randint(LOWER_LIMIT, UPPER_LIMIT)
+
+
+def generate_safe_prime():
+    q = 0
+    while True:
+        q = random_big_num()
+        while q % 12 != 5:
+            q = random_big_num()
+            # print(q)
+        # print("Testing if prime")
+        if test_prime(q):
+            # print("We found our prime")
+            p = 2 * q + 1
+            if test_prime(p):
+                return p
+
+
+def prompt():
+    try:
+        print("What would you like to do?")
+        print("1. Key Generation")
+        print("2. Encryption")
+        print("3. Decryption")
+        return int(input("Enter 1-3: "))
+    except:
+        print("Only enter a number 1-3")
+        return prompt()
+
+
+def main():
+    selection = prompt()
+    if selection == 1:
+        seed_num = int(input("Input a number to seed the random number generator: "))
+        random.seed(seed_num)
+        p = generate_safe_prime()
+        g = 2
+        e = 12
+        pubkeyfile = open("pubkey.txt", 'w')
+        pubkeyfile.write(str(p) + " " +  str(g) + " " + str(e))
+        pubkeyfile.close()
+        print(p)
+    elif selection == 2:
+        print("encryption")
+    elif selection == 2:
+        print("encryption")
+    else:
+        print("Invalid selection. Please enter only 1-3.")
+
+
+main()
